b40dc162b3ca8753fcdaa5b03d1842506bed764f
Sitt Aung <sittaung07@gmail.com>
2020-07-19 13:24:51 +0630

    Add modified custom views - Custom Attribute Rounded Corner Image

--- app/src/main/java/com/sa/padc_x_customviewcomponent_sa/views/components/RoundedCornerImageView.kt
+++ app/src/main/java/com/sa/padc_x_customviewcomponent_sa/views/components/RoundedCornerImageView.kt
@@ -6,14 +6,22 @@ import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.withStyledAttributes
+import com.sa.padc_x_customviewcomponent_sa.R
 
 class RoundedCornerImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    private var cornerRadius = 32f
+    private var cornerRadius = 0f
 
     private val path = Path()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.RoundedCornerImageView) {
+            cornerRadius = getDimension(R.styleable.RoundedCornerImageView_cornerRadius, 0f)
+        }
+    }
 
     override fun onDraw(canvas: Canvas?) {
         val rectangle = RectF(0f, 0f, width.toFloat(), height.toFloat())
